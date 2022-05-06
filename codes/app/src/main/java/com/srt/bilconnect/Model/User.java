b41ea1a0767aca778156dbdcfcac3396d4b92cf3
SertacD <sertac1902@gmail.com>
2022-05-06 14:46:05 +0300

    Merged

--- codes/app/src/main/java/com/srt/bilconnect/Model/User.java
+++ codes/app/src/main/java/com/srt/bilconnect/Model/User.java
@@ -14,6 +14,7 @@ public class User implements Followable{
     private ArrayList<User> followers;
     private ArrayList<Followable> followedAccounts;
     private ArrayList<User> blockedAccounts;
+    private ArrayList<Interest> interests;
     private ArrayList<Event> pastEvents;
 //interestleri ekle
 //eventleri ekle
@@ -31,6 +32,54 @@ public class User implements Followable{
         this.bilkentID = bilkentID;
         this.questionAnswers = questionAnswers;
         this.department = department;
+    }
+
+    public String getBio() {
+        return bio;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    public ArrayList<User> getFollowers() {
+        return followers;
+    }
+
+    public void setBio(String bio) {
+        this.bio = bio;
+    }
+
+    public void setRating(double rating) {
+        this.rating = rating;
+    }
+
+    public void setFollowers(ArrayList<User> followers) {
+        this.followers = followers;
+    }
+
+    public void setFollowedAccounts(ArrayList<Followable> followedAccounts) {
+        this.followedAccounts = followedAccounts;
+    }
+
+    public void setBlockedAccounts(ArrayList<User> blockedAccounts) {
+        this.blockedAccounts = blockedAccounts;
+    }
+
+    public void setInterests(ArrayList<Interest> interests) {
+        this.interests = interests;
+    }
+
+    public ArrayList<Followable> getFollowedAccounts() {
+        return followedAccounts;
+    }
+
+    public ArrayList<User> getBlockedAccounts() {
+        return blockedAccounts;
+    }
+
+    public ArrayList<Interest> getInterests() {
+        return interests;
     }
 
     public String getUserID() {
---
+++ codes/app/src/main/java/com/srt/bilconnect/View/Fragments/HomeFragment.java
@@ -0,0 +1,4 @@
+package com.srt.bilconnect.View.Fragments;
+
+public class HomeFragment {
+}
