448e964b9b1cf2fbd278d96058f0452067448a08
bilginer <bilgineroral@gmail.com>
2022-05-09 22:01:17 +0300

    fixed

--- codes/app/src/main/java/com/srt/bilconnect/Model/User.java
+++ codes/app/src/main/java/com/srt/bilconnect/Model/User.java
@@ -28,13 +28,11 @@ public class User implements Followable{
 
 //eventleri ekle
 
-    public User() {//boş user constructoru firebase için gerekli
-        // test icin koydum..(Bilginer)
-
-        pastEvents = new ArrayList<>();
+    public User() {
+        /*pastEvents = new ArrayList<>();
         pastEvents.add(new Event("takilmaca", new User(), 12));
         pastEvents.add(new Event("ogle yemegi", new User(), 4));
-        pastEvents.add(new Event("kodlama eventi", new User(), 6));
+        pastEvents.add(new Event("kodlama eventi", new User(), 6));*/
     }
 
     public User(String username, String userID, String email, String bilkentID, String department) {
