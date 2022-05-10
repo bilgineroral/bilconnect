26e2a5e1efa951f36fb65c4841329cf78360cc38
Serhat Yılmaz <serhaty@ug.bilkent.edu.tr>
2022-05-10 17:40:37 +0300

    Auto stash before merge of "main" and "origin/main"

--- codes/app/src/main/java/com/srt/bilconnect/View/Fragments/MapFragment.java
+++ codes/app/src/main/java/com/srt/bilconnect/View/Fragments/MapFragment.java
@@ -13,6 +13,8 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import com.srt.bilconnect.Model.Event;
+import com.srt.bilconnect.Model.Place;
 import com.srt.bilconnect.View.PlaceActivities.EventsAtSelectedLocation;
 import com.srt.bilconnect.databinding.FragmentMapBinding;
 
@@ -31,12 +33,27 @@ public class MapFragment extends Fragment {
     ImageView imageView4;
     ImageView imageView5;
 
+    public Place place1;
+    public Place place2;
+    public Place place3;
+    public Place place4;
+    public Place place5;
+
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
 
         binding = FragmentMapBinding.inflate(inflater, container, false);
         View view = binding.getRoot();
+
+        place1 = new Place();
+        place2 = new Place();
+        place3 = new Place();
+        place4 = new Place();
+        place5 = new Place();
+
+        place3.getUpcomingEvents().add(new Event());
+        place4.getUpcomingEvents().add(new Event());
 
         textViewFirst = binding.textView29;
         textViewSecond = binding.textView6;
@@ -50,7 +67,9 @@ public class MapFragment extends Fragment {
         imageView4 = binding.imageView18;
         imageView5 = binding.imageView17;
 
-        /*
+        textViewThird.setText("" + place3.getUpcomingEvents().size());
+        textViewFourth.setText("" + place4.getUpcomingEvents().size());
+
         imageView1.setVisibility(View.INVISIBLE);
         imageView2.setVisibility(View.INVISIBLE);
         imageView3.setVisibility(View.INVISIBLE);
@@ -63,7 +82,22 @@ public class MapFragment extends Fragment {
         textViewFourth.setVisibility(View.INVISIBLE);
         textViewFifth.setVisibility(View.INVISIBLE);
 
-         */
+
+        if(Integer.parseInt(textViewFirst.getText().toString()) != 0 || place1.getUpcomingEvents().size() != 0) {
+            imageView1.setVisibility(View.VISIBLE);
+            textViewFirst.setVisibility(View.VISIBLE); }
+        if(Integer.parseInt(textViewSecond.getText().toString()) != 0 || place2.getUpcomingEvents().size() != 0) {
+            imageView2.setVisibility(View.VISIBLE);
+            textViewSecond.setVisibility(View.VISIBLE);}
+        if(Integer.parseInt(textViewThird.getText().toString()) != 0 || place3.getUpcomingEvents().size() != 0) {
+            imageView3.setVisibility(View.VISIBLE);
+            textViewThird.setVisibility(View.VISIBLE);}
+        if(Integer.parseInt(textViewFourth.getText().toString()) != 0 || place4.getUpcomingEvents().size() != 0) {
+            imageView4.setVisibility(View.VISIBLE);
+            textViewFourth.setVisibility(View.VISIBLE);}
+        if(Integer.parseInt(textViewFifth.getText().toString()) != 0 || place5.getUpcomingEvents().size() != 0) {
+            imageView5.setVisibility(View.VISIBLE);
+            textViewFifth.setVisibility(View.VISIBLE);}
 
 
 
@@ -111,6 +145,16 @@ public class MapFragment extends Fragment {
         super.onDestroyView();
         binding = null;
     }
+
+    public Place getPlace1() { return place1; }
+
+    public Place getPlace2() { return place2; }
+
+    public Place getPlace3() { return place3; }
+
+    public Place getPlace4() { return place4; }
+
+    public Place getPlace5() { return place5; }
 
 
 }
