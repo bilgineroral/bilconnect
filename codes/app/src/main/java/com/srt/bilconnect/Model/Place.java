618867067f8df483a29586989d45e047a74c1b24
Serhat Yılmaz <serhaty@ug.bilkent.edu.tr>
2022-05-10 19:46:58 +0300

    Auto stash before merge of "main" and "origin/main"

--- codes/app/src/main/java/com/srt/bilconnect/Model/Place.java
+++ codes/app/src/main/java/com/srt/bilconnect/Model/Place.java
@@ -8,31 +8,48 @@ import java.util.ArrayList;
 public class Place{
 
     private String placeName;
-    private double rating = 0;
-    private Point coordinates;
-    private ArrayList<User> followers;
+    //private ArrayList<User> followers;
     private ArrayList<Event> upcomingEvents;
+    //public static String[] placeNames = {"81", "Mayfest", "Odeon", "Bilkent Center", "East Campus"};
+
+    public Place() {}
 
     public Place(String placeName) {
         this.placeName = placeName;
-        followers = new ArrayList<>();
+        //followers = new ArrayList<>();
         upcomingEvents = new ArrayList<>();
     }
+
+    //public static String[] getPlaceNames() {return placeNames;}
+
+    //public static void setPlaceNames(String[] placeNames) {Place.placeNames = placeNames;}
 
     public void setPlaceName(String placeName) { this.placeName = placeName; }
 
     public String getPlaceName() { return this.placeName; }
 
+    /*public ArrayList<User> getFollowers() {
+        return followers;
+    }*/
 
+    /*
+    public void setFollowers(ArrayList<User> followers) {
+        this.followers = followers;
+    }*/
+
+    public void setUpcomingEvents(ArrayList<Event> upcomingEvents) {
+        this.upcomingEvents = upcomingEvents;
+    }
+/*
     public void addFollower(Followable anObject) {
         followers.add((User) anObject);
-    }
+    }*/
 
     public ArrayList<Event> getUpcomingEvents() {
         return this.upcomingEvents;
     }
-
+/*
     public void removeFollower(Followable anObject) {
         followers.remove((User) anObject);
-    }
+    }*/
 }
