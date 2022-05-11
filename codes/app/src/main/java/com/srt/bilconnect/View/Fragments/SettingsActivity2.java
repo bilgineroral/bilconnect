36183192512f02ef96063a86c77cca7dfefe877a
Kaan <hkaanoktay@hotmail.com>
2022-05-11 20:27:50 +0300

    change password button works now

--- codes/app/src/main/java/com/srt/bilconnect/View/Fragments/SettingsActivity2.java
+++ codes/app/src/main/java/com/srt/bilconnect/View/Fragments/SettingsActivity2.java
@@ -2,15 +2,35 @@ package com.srt.bilconnect.View.Fragments;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
+import com.srt.bilconnect.EditInterests;
 import com.srt.bilconnect.R;
+import com.srt.bilconnect.View.AdditionalInfoActivity;
+import com.srt.bilconnect.View.ChangePasswordActivity;
+import com.srt.bilconnect.databinding.FragmentProfileBinding;
+
 
 public class SettingsActivity2 extends AppCompatActivity {
+    TextView textView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings2);
+
+        textView = findViewById(R.id.textView5);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SettingsActivity2.this, ChangePasswordActivity.class);
+                startActivity(intent);
+            }
+
+        });
     }
 }
